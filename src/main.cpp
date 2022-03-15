a750d8815944c0b09968857e1ee266e291534095
Will Eccles <will@eccles.dev>
2022-03-15 10:45:37 -0400

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <clang/Format/Format.h>
+
+#include <iostream>
+
+namespace cf = clang::format;
+
+int main(int argc, char** argv) {
+  if (argc != 2) {
+    std::cerr << "missing style\n";
+    return 1;
+  }
+
+  cf::FormatStyle style{};
+
+
+  if (cf::getPredefinedStyle(argv[1], cf::FormatStyle::LanguageKind::LK_Cpp,
+                                        &style)) {
+    std::cout << cf::configurationAsText(style) << '\n';
+    return 0;
+  } else {
+    std::cerr << "unknown style: " << argv[1] << '\n';
+    return 1;
+  }
+
+  return 0;
+}
